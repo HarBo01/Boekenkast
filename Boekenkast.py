4bd5e25cf975a63886dc568d8d7f620f8229446c
MarHar <h.boekhorst@hetnet.nl>
2025-03-02 18:12:17 +0100

    Init

---
+++ Boekenkast.py
@@ -0,0 +1,61 @@
+# boekenkast = []
+
+# while True:
+#     boeken_titel = input("Boektitel invoeren in je boekenkast!")
+#     auteur_boeken = input("Schrijver boeken invoeren!")
+
+
+#     if boeken_titel or auteur_boeken == 'nee':
+#         break
+#     boekenkast.append (boeken_titel)
+    
+# print(boekenkast)
+
+# def boeken_kast (titel, schrijver):
+#     """Boekenlijst"""
+#     print("Titel boek " + titel)
+#     print("Schrijver " + schrijver)
+
+# boeken_kast('Parijs', 'harry')
+
+# AFBLIJVEN OP STRAFFE DES DOODS
+
+# mijn_boekenkast = []
+
+
+# while True:
+#     boek = input("Voer een boek in om aan de boekenkast toe te voegen (of typ 'Q' om te stoppen): ")
+#     if boek == 'q' or boek == 'Q':
+#         break
+#     mijn_boekenkast.append(boek)
+
+
+# print("Mijn boekenkast:", mijn_boekenkast)
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+
